657e81f92ccc835eec157f820e102939f375e239
ng42 <203120170+ng42-secondary@users.noreply.github.com>
2025-03-17 01:27:17 -0400

    WIP

--- BoxExamples/fast_blur/FastBlur.gd
+++ BoxExamples/fast_blur/FastBlur.gd
@@ -1,30 +1,42 @@
 extends BaseCensorBox
 
 static var frame_count = -1  # only one node will update the material
+static var blur_adjust: float = 0.0
+static var blur_dir = true
+const base_blur: float = 20.0
 
 func _ready():
 	super._ready()
+	$TextureRect/TextureRectX.size = $TextureRect.size
 	update_back_screen()
 
-func _blur_changed(radius: float):
-	$%BlurX.material.set_shader_parameter("radius", radius)
-	$%BlurY.material.set_shader_parameter("radius", radius)
-	$%BlurRadiusLabel.text = "%.2f" % [radius]
+func set_blur_radius(radius: float):
+	$TextureRect.material.set_shader_parameter("radius", radius)
+	$TextureRect/TextureRectX.material.set_shader_parameter("radius", radius)
 	
+func adjust_blur_sync():
+	if(blur_dir):
+		blur_adjust += 0.01
+		$TextureRect.material.set_shader_parameter("radius", base_blur + blur_adjust)
+		$TextureRect/TextureRectX.material.set_shader_parameter("radius", base_blur - blur_adjust)
+		if(blur_adjust >= 10):
+			blur_dir = false
+	else:
+		blur_adjust -= 0.01
+		$TextureRect.material.set_shader_parameter("radius", base_blur + blur_adjust)
+		$TextureRect/TextureRectX.material.set_shader_parameter("radius", base_blur - blur_adjust)
+		if(blur_adjust <= -10):
+			blur_dir = true
 	
 func update_back_screen():
 	var fc = Engine.get_process_frames()
 	if frame_count == fc:
 		return
 	frame_count = fc
+	adjust_blur_sync()
 	var current_screen_texture : ImageTexture = BetaData.screen_recorder.get_screen_texture()
-	var margin_x = MARGIN * box_detect[2]
-	var margin_y = MARGIN * box_detect[3]
-	position = Vector2(box_detect[0] - margin_x, box_detect[1] - margin_y)
-	$TextureRectY.size = Vector2(box_detect[2] + 2*margin_x, box_detect[3] + 2*margin_y)
-	$TextureRectY.texture = current_screen_texture
-	$TextureRectX.size = Vector2(box_detect[2] + 2*margin_x, box_detect[3] + 2*margin_y)
-	$TextureRectX.texture = current_screen_texture
+	$TextureRect.material.set_shader_parameter("back_screen_texture", current_screen_texture)
+	$TextureRect/TextureRectX.material.set_shader_parameter("back_screen_texture", current_screen_texture)
 	
 func _process(_delta):
 	# update material once per frame
